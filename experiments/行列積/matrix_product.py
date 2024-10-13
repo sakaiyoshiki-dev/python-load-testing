57c6d9c5b5cd3109d3f644d1e5b052587f2c5377
SAKAI Yoshiki <sakai@sakaiyoshiki.dev>
2024-10-13 16:00:04 +0900

    行列積の実験を実施。
    numbaの効果はほぼ見られず。

---
+++ experiments/行列積/matrix_product.py
@@ -0,0 +1,34 @@
+from typing import Callable
+import numpy as np
+from numba import njit
+import time
+
+
+@njit(cache=True)
+def repeat_matrix_products_jitted(matrix: np.ndarray, n_repeat: int) -> np.ndarray:
+    # 計測と高速化対象
+    m = matrix
+    for n in range(n_repeat):
+        # m = np.dot(m, matrix)  # scipy 0.16+ is required for linear algebra
+        m = m.dot(matrix)
+    return m
+
+
+def repeat_matrix_products(matrix: np.ndarray, n_repeat: int) -> np.ndarray:
+    # 計測と高速化対象
+    m = matrix
+    for n in range(n_repeat):
+        m = np.dot(m, matrix)
+    return m
+
+
+def experiments(experiment_sizes: list[tuple[int, int]], repeat_matrix_products: Callable):
+    perf_results = {}
+    for n_size, n_iter in experiment_sizes:
+        matrix = np.random.normal(loc=0.0, scale=0.01, size=(n_size, n_size))
+        start = time.perf_counter()
+        ret = repeat_matrix_products(matrix=matrix, n_repeat=n_iter)
+        end = time.perf_counter()  # 計測終了
+
+        perf_results[(n_size, n_iter)] = end - start
+        print(f"{perf_results[(n_size, n_iter)]=:.2f} sec.")
